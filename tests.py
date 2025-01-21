c80f93d67a43f09f016b5ef1681cdf3cfed0d592
yonatan.r@razor-labs.com <yonatan.r@razor-labs.com>
2025-01-22 01:36:37 +0200

    - started working with built in CIFAR10 dataset - Made first model class and experiment class - Added tests file - removed utiles file

--- tests.py
+++ tests.py
@@ -1,17 +1,14 @@
+import pytest
 import torch
 from torchvision import datasets, transforms
 from CNN import CNN
 
-def main():
+def test_CNN_sanity():
     dataset = datasets.CIFAR10(root='./data', train=True, transform=transforms.ToTensor())
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=True)
     images, labels = next(iter(dataloader))
     model = CNN()
     output = model(images)
-    print("hello")
-
-
-
-
-if __name__ == '__main__':
-    main()
+    # Assertions to check the output
+    assert output.shape == (64, 1), f"Expected output shape (64, 1), but got {output.shape}"
+    assert (0 <= output).all() and (output <= 1).all(), "Output values should be between 0 and 1 (after sigmoid)"
