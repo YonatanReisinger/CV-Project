ff9a92b79a7579dd7f46c4a8a31d11195f9a9167
yonatan.r@razor-labs.com <yonatan.r@razor-labs.com>
2025-01-27 19:53:56 +0200

    - Did exp6 - Added a new CNN with different fully connected archi

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch
 from torchvision import datasets, transforms
 from CNN import CNN
+from CNN_V2 import CNN_V2
 from Experiment import Experiment
 from typing import List, Callable
 import random
@@ -122,13 +123,14 @@ def experiment_architectures_with_depth_4_new():
                              probability = 1)
 
 def load_experiments(directory="."):
-    pkl_files = [f for f in os.listdir(directory) if f.endswith(".pkl") and f.startswith("Experiment_4_") and "2025-01-25" in f]
+    pkl_files = [f for f in os.listdir(directory) if f.endswith(".pkl") and f.startswith("Experiment_5_")]
     loaded_data = {}
 
     for pkl_file in pkl_files:
         file_path = os.path.join(directory, pkl_file)
         try:
             loaded_data[pkl_file] = Experiment.from_pickle(file_path)
+            print(loaded_data[pkl_file])
         except Exception as e:
             print(f"Failed to load {pkl_file}: {e}")
 
@@ -244,16 +246,173 @@ def experiment5_5():
     exp.to_pickle()
     print(exp)
 
+def experiment6_1():
+    # bigger pooling, more epochs
+    model = CNN(convolution_layers=[3, 32, 64, 128],
+                kernel_sizes=[5, 5, 5],
+                strides=[1, 1, 1],
+                paddings=[2, 2, 2],
+                max_pool_kernel_sizes=[3, 3, 3],
+                output_size=10,
+                p_conv=0.2,
+                p_fc=0.5)
+    exp = Experiment(model=model,
+                     criterion=torch.nn.CrossEntropyLoss(),
+                     batch_size=100,
+                     epochs=300,
+                     lr=0.1,
+                     momentum=0.2,
+                     optimizer_name="SGD")
+
+    exp()
+    exp.to_pickle()
+    print(exp)
+
+def experiment6_2():
+    # Higher learning rate
+    model = CNN(convolution_layers=[3, 32, 64, 128],
+                kernel_sizes=[5, 5, 5],
+                strides=[1, 1, 1],
+                paddings=[2, 2, 2],
+                max_pool_kernel_sizes=[2, 2, 2],
+                output_size=10,
+                p_conv=0.2,
+                p_fc=0.5)
+    exp = Experiment(model=model,
+                     criterion=torch.nn.CrossEntropyLoss(),
+                     batch_size=100,
+                     epochs=200,
+                     lr=0.2,
+                     momentum=0.2,
+                     optimizer_name="SGD")
+
+    exp()
+    exp.to_pickle()
+    print(exp)
+
+def experiment6_3():
+    # Much Higher momentum
+    model = CNN(convolution_layers=[3, 32, 64, 128],
+                kernel_sizes=[5, 5, 5],
+                strides=[1, 1, 1],
+                paddings=[2, 2, 2],
+                max_pool_kernel_sizes=[2, 2, 2],
+                output_size=10,
+                p_conv=0.2,
+                p_fc=0.5)
+    exp = Experiment(model=model,
+                     criterion=torch.nn.CrossEntropyLoss(),
+                     batch_size=100,
+                     epochs=200,
+                     lr=0.1,
+                     momentum=0.9,
+                     optimizer_name="SGD")
+
+    exp()
+    exp.to_pickle()
+    print(exp)
+
+def experiment6_4():
+    # Higher momentum
+    model = CNN(convolution_layers=[3, 32, 64, 128],
+                kernel_sizes=[5, 5, 5],
+                strides=[1, 1, 1],
+                paddings=[2, 2, 2],
+                max_pool_kernel_sizes=[2, 2, 2],
+                output_size=10,
+                p_conv=0.2,
+                p_fc=0.5)
+    exp = Experiment(model=model,
+                     criterion=torch.nn.CrossEntropyLoss(),
+                     batch_size=100,
+                     epochs=200,
+                     lr=0.1,
+                     momentum=0.4,
+                     optimizer_name="SGD")
+
+    exp()
+    exp.to_pickle()
+    print(exp)
+
+def experiment6_5():
+    # bigger batch size
+    model = CNN(convolution_layers=[3, 32, 64, 128],
+                kernel_sizes=[5, 5, 5],
+                strides=[1, 1, 1],
+                paddings=[2, 2, 2],
+                max_pool_kernel_sizes=[2, 2, 2],
+                output_size=10,
+                p_conv=0.2,
+                p_fc=0.5)
+    exp = Experiment(model=model,
+                     criterion=torch.nn.CrossEntropyLoss(),
+                     batch_size=256,
+                     epochs=200,
+                     lr=0.1,
+                     momentum=0.2,
+                     optimizer_name="SGD")
+
+    exp()
+    exp.to_pickle()
+    print(exp)
+
+def experiment6_6():
+    model = CNN_V2(convolution_layers=[3, 32, 64, 128],
+                kernel_sizes=[5, 5, 5],
+                strides=[1, 1, 1],
+                paddings=[2, 2, 2],
+                max_pool_kernel_sizes=[2, 2, 2],
+                output_size=10,
+                p_conv=0.2,
+                p_fc=0.5)
+    exp = Experiment(model=model,
+                     criterion=torch.nn.CrossEntropyLoss(),
+                     batch_size=100,
+                     epochs=200,
+                     lr=0.1,
+                     momentum=0.2,
+                     optimizer_name="SGD")
+
+    exp()
+    exp.to_pickle()
+    print(exp)
+
+def experiment6_7():
+    # smaller batch size
+    model = CNN(convolution_layers=[3, 32, 64, 128],
+                kernel_sizes=[5, 5, 5],
+                strides=[1, 1, 1],
+                paddings=[2, 2, 2],
+                max_pool_kernel_sizes=[2, 2, 2],
+                output_size=10,
+                p_conv=0.2,
+                p_fc=0.5)
+    exp = Experiment(model=model,
+                     criterion=torch.nn.CrossEntropyLoss(),
+                     batch_size=64,
+                     epochs=200,
+                     lr=0.1,
+                     momentum=0.2,
+                     optimizer_name="SGD")
+
+    exp()
+    exp.to_pickle()
+    print(exp)
+
 def main():
-    experiment5_2()
-    experiment5_3()
-    experiment5_4()
-    experiment5_5()
+    experiment6_1()
+    experiment6_2()
+    experiment6_3()
+    experiment6_4()
+    experiment6_5()
+    experiment6_6()
+    experiment6_7()
 
 
 if __name__ == '__main__':
-    main()
+    # main()
     # experiment_shallow_architectures()
-    # exp = Experiment.from_pickle("Experiment_5_78.54_2025-01-26_02:03_SGD.pkl")
+    # exp = Experiment.from_pickle("Experiment_7_81.88_2025-01-27_16:28_SGD.pkl")
     # exp.plot_loss()
     # print(exp)
+    load_experiments()
